8556e6ccc1e4ea68729d254bd6ceb71093b6441b
imtiajul-islam-d <imtiajul@hotmail.com>
2023-02-19 19:38:26 +0600

    Redux part 1

---
+++ script.js
@@ -0,0 +1,45 @@
+const showCount = document.querySelector("#count");
+const inc = document.querySelector("#inc");
+const dec = document.querySelector("#dec");
+
+// create a initial state
+const initialState = {
+  value: 0,
+};
+
+function counterReducer (state = initialState, action) {
+  if(action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    }
+  }else if(action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1
+    }
+  } else {
+    return state
+  }
+}
+
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+  const state = store.getState();
+  showCount.innerText = state.value.toString();
+};
+
+render();
+store.subscribe(render);
+
+inc.addEventListener("click", () => {
+  store.dispatch({
+    type: "increment",
+  });
+});
+dec.addEventListener("click", () => {
+  store.dispatch({
+    type: "decrement",
+  });
+});
